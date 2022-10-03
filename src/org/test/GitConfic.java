b013e3c95b4d01d806c086e22d7c53ccc385c2fd
kumaresh <kumareshkumareee75@gmail.com>
2022-10-03 10:37:40 +0530

    dan

--- src/org/test/GitConfic.java
+++ src/org/test/GitConfic.java
@@ -2,12 +2,13 @@ package org.test;
 
 public class GitConfic {
 	private void method1() {
-	System.out.println("method1");
+	System.out.println("method3");
+	
 
 	}
 	
 	private void m2() {
-		System.out.println("method2");
+		System.out.println("method21");
 	}
 }
 
